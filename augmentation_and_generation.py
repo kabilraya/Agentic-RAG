72677715c8d102af4d700458666b62528885a33c
kabilraya <roykabil99@gmail.com>
2025-10-31 16:46:00 +0545

    edited the system prompt slightly for better answers

--- augmentation_and_generation.py
+++ augmentation_and_generation.py
@@ -87,7 +87,9 @@ def chat(prompt:str):
     Your task is to help the user(a probable customer to find a laptop according to his/her need). Follow this instructions:
     1. First, call the "query_generation" tool wit the user's query.
     2. After you get the list of sub_queries, you MUST call the 'deep_thinking_retrieval' tool using that list as a parameter.
-    3. Finally, use the retrieved documents to answer the user's original query.
+    3. Finally, use the retrieved documents to answer all the queries such as price comparision, specification comparision, X VS Y Laptop Brands where X is the laptop searched by the user, Use cases(Gaming, Light Weight use-case)
+    4. Include all the possible answers analysing the retrieved docs and the all the queries generated. 
+    5. Give a detailed answer that covers all the concerns of the user without him/her having to explain much in their query.
     
     User Query: "{prompt}"
     """
